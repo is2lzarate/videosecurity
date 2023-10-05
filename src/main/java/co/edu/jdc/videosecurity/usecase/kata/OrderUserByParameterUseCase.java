97d121eaa7db9d4b13868d4942cb1f507dee3055
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-10-05 10:50:28 -0500

    ✨Test approved

--- src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
+++ src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
@@ -2,7 +2,9 @@ package co.edu.jdc.videosecurity.usecase.kata;
 
 import co.edu.jdc.videosecurity.domain.entities.User;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderUserByParameterUseCase {
 
@@ -12,7 +14,48 @@ public class OrderUserByParameterUseCase {
         //TODO Si el parametro es null debemos retornar la lista tal cual como esta
         //TODO Si la lista es null debemos retornar null
         //TODO Si la lista esta vacia debemos retornar la lista tal cual como esta
+        if (users == null || users.isEmpty()) {
+            return users;
+        }
         //TODO Si el parametro es vacio debemos retornar la lista tal cual como esta
+        if (parameter == null || parameter.isEmpty()) {
+            return users;
+        }
+
+        if (parameter.equals("firstName") && asc) {
+            users.sort(Comparator.comparing(User::getFirstName));
+            return users;
+        }
+        if (parameter.equals("firstName")) {
+            users.sort(Comparator.comparing(User::getFirstName).reversed());
+            return users;
+        }
+        if (parameter.equals("firstLastName") && asc) {
+            users.sort(Comparator.comparing(User::getFirstLastName));
+            return users;
+        }
+        if (parameter.equals("firstLastName")) {
+            users.sort(Comparator.comparing(User::getFirstLastName).reversed());
+            return users;
+        }
+        if (parameter.equals("email") && asc) {
+            users.sort(Comparator.comparing(User::getEmail));
+            return users;
+        }
+        if (parameter.equals("email")) {
+            users.sort(Comparator.comparing(User::getEmail).reversed());
+            return users;
+        }
+        if (parameter.equals("other") && asc) {
+            users.sort(Comparator.comparing(User::getEmail));
+            return users;
+        }
+        if (parameter.equals("other")) {
+            users.sort(Comparator.comparing(User::getEmail).reversed());
+            return users;
+        }
+
+
         //TODO El parametro a ordenar puede ser firstName, firstLastName o email unicamente
         //TODO Si el parametro es firstName debemos ordenar por el primer nombre
         //TODO Si el parametro es firstLastName debemos ordenar por el primer apellido
