b216aef59c30fc58ba80454ca5638caca28feee4
etruiz-github <142608350+etruiz-github@users.noreply.github.com>
2023-10-06 10:25:53 -0500

    :white_check_mark:

--- src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
+++ src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
@@ -7,75 +7,17 @@ import java.util.List;
 
 public class OrderUserByParameterUseCase {
 
-    public List<User> orderUsersByParameter(List<User> users, String parameter, boolean asc){
-        //TODO Debemos construir la logica para ordenar los usuarios por el parametro que llega en parameter y de forma ascendente o descendente segun el booleano
+    public List<User> orderUsersByParameter(List<User> users, String parameter, boolean asc) {
 
-        //TODO Si el parametro no existe debemos retornar la lista tal cual como esta
-        //TODO Si el parametro es null debemos retornar la lista tal cual como esta
-        //TODO Si la lista es null debemos retornar null
-        if(validateTest(users, parameter, asc))
+        if (parameter == null || parameter.isEmpty() || users == null || users.isEmpty())
             return users;
-
-        if (asc){
-            if (parameter == "firstName"){
-                users.sort((a,b) -> a.getFirstName().compareTo(b.getFirstName()));
-                return users;
-            }
-        }else{
-            if (parameter == "firstName"){
-                users.sort((a,b) -> b.getFirstName().compareTo(a.getFirstName()));
-                return users;
-            }
-        }
-
-        if (asc){
-            if (parameter == "firstLastName"){
-                users.sort((a,b) -> a.getFirstLastName().compareTo(b.getFirstLastName()));
-                return users;
-            }
-        }else{
-            if (parameter == "firstLastName"){
-                users.sort((a,b) -> b.getFirstLastName().compareTo(a.getFirstLastName()));
-                return users;
-            }
-        }
-
-        if (asc){
-            if (parameter == "email"){
-                users.sort((a,b) -> a.getEmail().compareTo(b.getEmail()));
-                return users;
-            }
-        }else{
-            if (parameter == "email"){
-                users.sort((a,b) -> b.getEmail().compareTo(a.getEmail()));
-                return users;
-            }
-        }
-
-        if (!(parameter == "firstName" ||parameter == "firstLastName" || parameter == "email")){
-            return users;
-        }
-        //TODO El parametro a ordenar puede ser firstName, firstLastName o email unicamente
-        //TODO Si el parametro es firstName debemos ordenar por el primer nombre
-        //TODO Si el parametro es firstLastName debemos ordenar por el primer apellido
-        //TODO Si el parametro es email debemos ordenar por el email
-        //TODO Si el parametro es otro debemos retornar la lista tal cual como esta
-        return null;
+        if (parameter == "firstName")
+            users.sort((a, b) -> asc ? a.getFirstName().compareTo(b.getFirstName()) : b.getFirstName().compareTo(a.getFirstName()));
+        else if (parameter == "firstLastName")
+            users.sort((a, b) -> asc ? a.getFirstLastName().compareTo(b.getFirstLastName()) : b.getFirstLastName().compareTo(a.getFirstLastName()));
+        else if (parameter == "email")
+            users.sort((a, b) -> asc ? a.getEmail().compareTo(b.getEmail()) : b.getEmail().compareTo(a.getEmail()));
+        return users;
     }
 
-    private boolean validateTest(List<User> users, String parameter, boolean asc){
-
-        if(parameter == null || parameter.isEmpty()){
-            return true;
-        }
-
-        if(users == null || users.isEmpty()){
-            return true;
-        }
-
-
-
-        return false;
-
-    }
 }
