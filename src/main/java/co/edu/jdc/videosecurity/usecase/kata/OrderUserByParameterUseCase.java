f1a6c9d5b2bc4016f387f09f1a8f2e54cef744b5
unknown <dlaura@jdc.edu.co>
2023-10-06 10:20:54 -0500

    :recycle  codigo refactorizado

--- src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
+++ src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
@@ -2,58 +2,21 @@ package co.edu.jdc.videosecurity.usecase.kata;
 
 import co.edu.jdc.videosecurity.domain.entities.User;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class OrderUserByParameterUseCase {
 
+
     public List<User> orderUsersByParameter(List<User> users, String parameter, boolean asc) {
         List<User> userList = users;
-
-        if (userList == null || userList.isEmpty() || parameter == null || parameter.isEmpty()) {
+        if (userList == null || userList.isEmpty() || parameter == null || parameter.isEmpty())
             return userList;
-        }
-        if (asc) {
-            userList.sort(Comparator.comparing(User::getFirstName));
-        } else {
-            userList.sort(Comparator.comparing(User::getFirstName).reversed());
-        }
-
-        if (userList == null || userList.isEmpty()) {
-            return userList;
-        }
-
-        if (parameter == null || parameter.isEmpty()) {
-            return userList;
-        }
-
-        if (parameter.equals("firstLastName")) {
-            if (asc) {
-                userList.sort(Comparator.comparing(User::getFirstLastName));
-            } else {
-                userList.sort(Comparator.comparing(User::getFirstLastName).reversed());
-            }
-        }
-        if (parameter.equals("email")) {
-            if (asc) {
-                userList.sort(Comparator.comparing(User::getEmail));
-            } else {
-                userList.sort(Comparator.comparing(User::getEmail).reversed());
-            }
-        }
-
-        if (parameter.equals("firstName")) {
-            if (asc) {
-                userList.sort(Comparator.comparing(User::getFirstName));
-            } else {
-                userList.sort(Comparator.comparing(User::getFirstName).reversed());
-            }
-        }
-
+        if (parameter.equals("firstLastName") )
+            userList.sort(asc ? Comparator.comparing(User::getFirstLastName) : Comparator.comparing(User::getFirstLastName).reversed());
+        else if (parameter.equals("email"))
+            userList.sort(asc ? Comparator.comparing(User::getEmail): Comparator.comparing(User::getEmail).reversed());
+        else if (parameter.equals("firstName"))
+            userList.sort(asc ? Comparator.comparing(User::getFirstName): Comparator.comparing(User::getFirstName).reversed());
         return userList;
     }
-
-
-
 }
