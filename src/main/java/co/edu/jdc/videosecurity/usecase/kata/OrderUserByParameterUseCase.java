0df879652e55afd4c28d93e6b2af9d972bcf1623
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-10-10 13:15:36 -0500

    Refactor code :hammer:

--- src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
+++ src/main/java/co/edu/jdc/videosecurity/usecase/kata/OrderUserByParameterUseCase.java
@@ -4,64 +4,22 @@ import co.edu.jdc.videosecurity.domain.entities.User;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class OrderUserByParameterUseCase {
 
     public List<User> orderUsersByParameter(List<User> users, String parameter, boolean asc){
-        //TODO Debemos construir la logica para ordenar los usuarios por el parametro que llega en parameter y de forma ascendente o descendente segun el booleano
-        //TODO Si el parametro no existe debemos retornar la lista tal cual como esta
-        //TODO Si el parametro es null debemos retornar la lista tal cual como esta
-        //TODO Si la lista es null debemos retornar null
-        //TODO Si la lista esta vacia debemos retornar la lista tal cual como esta
-        if (users == null || users.isEmpty()) {
+        if (users == null || users.isEmpty() || parameter == null || parameter.isEmpty() || parameter.equals("other")) {
             return users;
         }
-        //TODO Si el parametro es vacio debemos retornar la lista tal cual como esta
-        if (parameter == null || parameter.isEmpty()) {
-            return users;
-        }
-
-        if (parameter.equals("firstName") && asc) {
-            users.sort(Comparator.comparing(User::getFirstName));
-            return users;
-        }
-        if (parameter.equals("firstName")) {
-            users.sort(Comparator.comparing(User::getFirstName).reversed());
-            return users;
-        }
-        if (parameter.equals("firstLastName") && asc) {
-            users.sort(Comparator.comparing(User::getFirstLastName));
-            return users;
-        }
-        if (parameter.equals("firstLastName")) {
-            users.sort(Comparator.comparing(User::getFirstLastName).reversed());
-            return users;
-        }
-        if (parameter.equals("email") && asc) {
-            users.sort(Comparator.comparing(User::getEmail));
-            return users;
-        }
-        if (parameter.equals("email")) {
-            users.sort(Comparator.comparing(User::getEmail).reversed());
-            return users;
-        }
-        if (parameter.equals("other") && asc) {
-            users.sort(Comparator.comparing(User::getEmail));
-            return users;
-        }
-        if (parameter.equals("other")) {
-            users.sort(Comparator.comparing(User::getEmail).reversed());
-            return users;
-        }
-
-
-        //TODO El parametro a ordenar puede ser firstName, firstLastName o email unicamente
-        //TODO Si el parametro es firstName debemos ordenar por el primer nombre
-        //TODO Si el parametro es firstLastName debemos ordenar por el primer apellido
-        //TODO Si el parametro es email debemos ordenar por el email
-        //TODO Si el parametro es otro debemos retornar la lista tal cual como esta
-        return null;
+        Comparator<User> comparator = switch (parameter) {
+            case "firstName" -> Comparator.comparing(User::getFirstName);
+            case "firstLastName" -> Comparator.comparing(User::getFirstLastName);
+            case "email" -> Comparator.comparing(User::getEmail);
+            default -> null;
+        };
+        users.sort(asc ? comparator : Objects.requireNonNull(comparator).reversed());
+        return users;
     }
-
 }
